29a6eb0de9b922baf91986b4a78053dfd570130c
luis <foreverluiscastro@gmail.com>
2021-08-25 02:06:12 -0400

    created and migrated database.

--- db/migrate/20210825022708_create_trips.rb
+++ db/migrate/20210825022708_create_trips.rb
@@ -1,6 +1,7 @@
 class CreateTrips < ActiveRecord::Migration[6.1]
   def change
     create_table :trips do |t|
+      t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20210825022729_create_destinations.rb
+++ db/migrate/20210825022729_create_destinations.rb
@@ -1,6 +1,9 @@
 class CreateDestinations < ActiveRecord::Migration[6.1]
   def change
     create_table :destinations do |t|
+      t.decimal :latitude
+      t.decimal :longitude
+      t.belongs_to :trip, null: false, foreign_key: true
 
       t.timestamps
     end
